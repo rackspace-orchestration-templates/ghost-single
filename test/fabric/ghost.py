c96af2d5a7f382cae8cfd2e2454f1492ad0fb513
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-09-10 19:24:32 -0500

    Adding assertion comments to tests in case of failure

--- test/fabric/ghost.py
+++ test/fabric/ghost.py
@@ -17,19 +17,19 @@ def ghost_is_responding():
 def check():
     env.platform_family = detect.detect()
 
-    assert package.installed("nginx")
-    assert package.installed("mysql-server-5.5")
-    assert file.exists("/usr/local/bin/node")
-    assert file.exists("/usr/local/bin/npm")
-    assert port.is_listening(80)
-    assert port.is_listening(2368)
-    assert port.is_listening(3306)
-    assert user.exists("ghost")
-    assert group.is_exists("ghost")
-    assert user.is_belonging_group("ghost", "www-data")
-    assert process.is_up("nginx")
-    assert process.is_up("node")
-    assert process.is_up("mysqld")
-    assert service.is_enabled("nginx")
-    assert service.is_enabled("mysqld")
+    assert package.installed("nginx"), 'nginx not installed'
+    assert package.installed("mysql-server-5.5"), 'mysql-server-5.5 not installed'
+    assert file.exists("/usr/local/bin/node"), 'node not found'
+    assert file.exists("/usr/local/bin/npm"), 'npm not found'
+    assert port.is_listening(80), '80/nginx is not listening'
+    assert port.is_listening(2368), '2368/node is not listening'
+    assert port.is_listening(3306), '3306/mysqld is not listening'
+    assert user.exists("ghost"), 'ghost user does not exist'
+    assert group.is_exists("ghost"), 'ghost group does not exist'
+    assert user.is_belonging_group("ghost", "www-data"), 'ghost user not in group www-data'
+    assert process.is_up("nginx"), 'nginx is not running'
+    assert process.is_up("node"), 'node is not running'
+    assert process.is_up("mysqld"), 'mysqld is not running'
+    assert service.is_enabled("nginx"), 'nginx service not enabled'
+    assert service.is_enabled("mysqld"), 'mysqld service not enabled'
     assert ghost_is_responding(), 'Ghost did not respond as expected'
