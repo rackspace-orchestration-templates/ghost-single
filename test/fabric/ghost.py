37aca6b307e81089c2dc1bc1b1826f51be67f50b
Jason Boyles <jason@duncancreek.net>
2015-02-06 12:53:56 -0600

    flake8 cleanup

--- test/fabric/ghost.py
+++ test/fabric/ghost.py
@@ -19,7 +19,8 @@ def check():
     env.platform_family = detect.detect()
 
     assert package.installed("nginx"), 'nginx not installed'
-    assert package.installed("mysql-server-5.5"), 'mysql-server-5.5 not installed'
+    assert package.installed("mysql-server-5.5"), \
+        'mysql-server-5.5 not installed'
     assert file.exists("/usr/local/bin/node"), 'node not found'
     assert file.exists("/usr/local/bin/npm"), 'npm not found'
     assert port.is_listening(80), '80/nginx is not listening'
@@ -27,7 +28,8 @@ def check():
     assert port.is_listening(3306), '3306/mysqld is not listening'
     assert user.exists("ghost"), 'ghost user does not exist'
     assert group.is_exists("ghost"), 'ghost group does not exist'
-    assert user.is_belonging_group("ghost", "www-data"), 'ghost user not in group www-data'
+    assert user.is_belonging_group("ghost", "www-data"), \
+        'ghost user not in group www-data'
     assert process.is_up("nginx"), 'nginx is not running'
     assert process.is_up("node"), 'node is not running'
     assert process.is_up("mysqld"), 'mysqld is not running'
